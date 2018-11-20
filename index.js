80925a972f6ae1a7b1cf873de755c7667f5ad131
sarah pai <pai.sarah89@gmail.com>
2018-11-20 07:47:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+function logDriverNames(driver) {
+  driver.forEach(function(driver){
+    console.log(driver.name);
+  });
+}
+
+function logDriversByHometown(driver, location) {
+  driver.forEach(function(driver) {
+    if (driver.hometown === location){
+      console.log(driver.name)
+    }
+  });
+}
+
+const driversByRevenue = function(drivers) {
+  const result = [...drivers]
+    return result.sort(function (a, b) {
+      return a["revenue"] - b["revenue"];
+  });
+}
+
+const driversByName = function(driver) {
+  const result = [...driver]
+  return result.sort(function(a, b){
+    return a["name"].localeCompare(b["name"]);
+  })
+}
+
+const totalRevenue = function(driver) {
+  const result = [...driver]
+  const counter = 0
+  return result.reduce(function(accumulator, currentValue){
+    return accumulator + currentValue["revenue"];
+  }, counter);
+}
+
+const averageRevenue = function(driver) {
+  return totalRevenue(driver) / driver.length
+};
