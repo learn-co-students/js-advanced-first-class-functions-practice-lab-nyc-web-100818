44006a099a11501435a073d73c702b43460a114e
tirempbs <tirem@fastmail.com>
2018-11-20 21:17:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+function logDriverNames(array) {
+  array.forEach(function(driver) {
+    console.log(driver.name)
+  })
+}
+
+function logDriversByHometown(array, location) {
+  array.forEach(function(driver) {
+    if (driver.hometown === location) {
+      console.log(driver.name)
+    }
+  })
+}
+
+function driversByRevenue(array) {
+  return array.slice().sort(function (a, b) {
+    return a.revenue - b.revenue;
+  })
+}
+
+function driversByName(array) {
+  return array.slice().sort(function(a,b) {
+    if (a.name < b.name)
+      return -1;
+    if (a.name > b.name)
+      return 1;
+    return 0;
+  })
+}
+
+function totalRevenue(array) {
+  const allRevenue = array.map(d => d.revenue)
+  return allRevenue.reduce(function(accumulator, currentValue, currentIndex, array) {
+    return accumulator + currentValue;
+  })
+}
+
+function averageRevenue(array) {
+  return totalRevenue(array) / array.length
+}
